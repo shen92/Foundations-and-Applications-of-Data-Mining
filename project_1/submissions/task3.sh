060213460450900ba153fd2a9d48151f2350aea9
Yingjie Shen <shen92@wisc.edu>
2021-09-21 00:02:39 -0700

    update sh

--- project_1/submissions/task3.sh
+++ project_1/submissions/task3.sh
@@ -1,5 +1,6 @@
 output_filepath_question_a="./outputs/task3_result_a.txt"
 output_filepath_question_b="./outputs/task3_result_b.json"
 
-rm -rf $output_filepath
+rm -rf $output_filepath_question_a
+rm -rf $output_filepath_question_b
 /home/local/spark/latest/bin/spark-submit --executor-memory 4G --driver-memory 4G task3.py $ASNLIB/publicdata/test_review.json $ASNLIB/publicdata/business.json $output_filepath_question_a $output_filepath_question_b
