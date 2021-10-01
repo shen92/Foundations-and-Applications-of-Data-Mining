c876f8de9f9be697ce960677b4b19a0d0231a429
Yingjie Shen <shen92@wisc.edu>
2021-10-01 03:16:09 -0700

    add test result

--- project_2/tests/task1.sh
+++ project_2/tests/task1.sh
@@ -12,7 +12,7 @@ output_case_2_path="./outputs/task1_result_case_2.txt"
 rm -rf $output_file_path
 rm -rf $output_case_1_path
 rm -rf $output_case_2_path
-# spark-submit --executor-memory 4G --driver-memory 4G task1.py $case_number $support $input_dev_file_path $output_file_path
+spark-submit --executor-memory 4G --driver-memory 4G task1.py $case_number $support $input_dev_file_path $output_file_path
 spark-submit --executor-memory 4G --driver-memory 4G task1.py 1 4 $input_test_file_path $output_case_1_path
 spark-submit --executor-memory 4G --driver-memory 4G task1.py 2 9 $input_test_file_path $output_case_2_path
-# open $output_file_path           
+open $output_file_path           
