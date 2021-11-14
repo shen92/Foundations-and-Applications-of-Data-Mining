c9443806aa471450e9d59c372116430a06741c07
Yingjie Shen <shen92@wisc.edu>
2021-11-14 05:23:57 -0800

    update test file

--- project_5/tests/task2.sh
+++ project_5/tests/task2.sh
@@ -5,6 +5,5 @@ output_filename="./outputs/task2_result.txt"
 
 rm -rf $output_filename
 python task2.py $input_filename $stream_size $num_of_asks $output_filename
-# python task2_Flajolet_Martin_Algorithm.py $input_filename $stream_size $num_of_asks $output_filename
 
 
